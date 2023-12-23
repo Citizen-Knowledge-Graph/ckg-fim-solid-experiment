1009c1b2f19aabc170b7ee53cad4d8bcba264d46
benjaminaaron <b.degenhart.work@gmail.com>
2023-12-23 14:00:30 +0100

    Add first solid code experiments

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # CKG FIM Solid Experiment
 
+## FIM
 
 ```sh
-node fim.js
+node fim.js # comment methods at the bottom in or out as needed
 ```
+
+## Solid
+
+### Setup
+
+Start the Community Solid Server:
+
+```shell
+npx @solid/community-server -c @css:config/file.json -f .solid
+```
+
+At first start, go to `localhost:3000` in the browser and:
+- *Sign up for an account* ...
+- *Create pod*, name it **ckg-pod**
+
+### Run
+
+```sh
+node solid.js # this will trigger a browser authorization
+```
+
+You may check your pod content using [penny.vincenttunru.com](https://penny.vincenttunru.com) or your local file system.
---
+++ solid.js
@@ -0,0 +1,48 @@
+const QueryEngine = require('@comunica/query-sparql-solid').QueryEngine;
+const { interactiveLogin } = require('solid-node-interactive-auth');
+
+const SERVER = "http://localhost:3000";
+const POD = "ckg-pod";
+const PROFILE_NAME = "main-profile";
+const PROFILE_URL = `${SERVER}/${POD}/${PROFILE_NAME}`;
+
+async function read() {
+    const session = await interactiveLogin({ oidcIssuer: SERVER });
+    console.log(session.info);
+    const engine = new QueryEngine();
+    let query = `
+        SELECT * WHERE { ?s ?p ?o } 
+    `;
+
+    const bindingsStream = await engine.queryBindings(query, {
+        sources: [PROFILE_URL],
+        '@comunica/actor-http-inrupt-solid-client-authn:session': session,
+    });
+
+    bindingsStream.on('data', (binding) => {
+        console.log(binding.toString());
+    });
+    bindingsStream.on('end', () => {
+        process.exit(0);
+    });
+    bindingsStream.on('error', (error) => {
+        console.error(error);
+    });
+}
+
+async function write() {
+    const session = await interactiveLogin({ oidcIssuer: SERVER });
+    const engine = new QueryEngine();
+    let query = `
+        PREFIX ckg: <http://ckg.de/default#>
+        INSERT DATA { <ckg:sub> <ckg:pred> <ckg:obj> }
+    `;
+
+    await engine.queryVoid(query, {
+        sources: [PROFILE_URL],
+        '@comunica/actor-http-inrupt-solid-client-authn:session': session,
+    });
+}
+
+read().then(() => {});
+// write().then(() => { process.exit(0); });
