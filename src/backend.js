09a6649c0058ac3f08cccd42ed8a7143d6a88537
benjaminaaron <b.degenhart.work@gmail.com>
2023-12-24 22:35:58 +0100

    Serve the lib via express static too

--- src/backend.js
+++ src/backend.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static("node_modules"));
 
 app.listen(PORT ,() => console.log("server is running at port " + PORT));
 
