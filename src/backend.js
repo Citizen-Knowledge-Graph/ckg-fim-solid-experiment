928de0d665578d4918f160865894428ab24ddf22
benjaminaaron <b.degenhart.work@gmail.com>
2023-12-24 22:52:45 +0100

    No more cors needed now

--- src/backend.js
+++ src/backend.js
@@ -1,9 +1,7 @@
 import express from "express";
-import cors from "cors";
 
 const PORT = 3030;
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static("node_modules"));
